ed1866e9ad5c18767c4e4afca3fe9f1fc3d22b67
fatihduzenli <fatihdznl1991@gmail.com>
2024-10-17 02:35:15 -0400

    bug fixed

---
+++ twitter-to-kafka-service/src/main/java/com/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,41 @@
+package com;
+
+import com.config.TwitterToKafkaServiceConfigData;
+import com.runner.StreamRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com")
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+
+    private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+    private final StreamRunner streamRunner;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData, StreamRunner streamRunner) {
+        this.twitterToKafkaServiceConfigData = twitterToKafkaServiceConfigData;
+        this.streamRunner = streamRunner;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("App starts");
+        LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[]{})));
+        LOG.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
+        streamRunner.start(); //Starts the stream runner when the application starts.
+    }
+}
