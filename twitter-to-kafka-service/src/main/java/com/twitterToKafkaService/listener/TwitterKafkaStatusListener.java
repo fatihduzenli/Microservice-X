e16d22b631a8aecf48c7fb84c770c655e493628e
fatihduzenli <fatihdznl1991@gmail.com>
2024-10-18 01:25:55 -0400

    TwitterKafkaStatusListener created

--- twitter-to-kafka-service/src/main/java/com/twitterToKafkaService/listener/TwitterKafkaStatusListener.java
+++ twitter-to-kafka-service/src/main/java/com/twitterToKafkaService/listener/TwitterKafkaStatusListener.java
@@ -1,8 +1,7 @@
-package com.listener;
+package com.twitterToKafkaService.listener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 import twitter4j.Status;
 import twitter4j.StatusAdapter;
