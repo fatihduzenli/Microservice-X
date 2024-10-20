9c9fdb434ad9e11c4bdffb51bad2f71324a75ad7
fatihduzenli <fatihdznl1991@gmail.com>
2024-10-20 16:30:22 -0400

    Twitter avro added

--- twitter-to-kafka-service/src/main/java/com/twitterToKafkaService/runner/StreamRunner.java
+++
@@ -1,9 +0,0 @@
-package com.twitterToKafkaService.runner;
-
-import twitter4j.TwitterException;
-
-public interface StreamRunner {//This interface defines a contract for classes that will run a stream (e.g., Twitter stream).
-
-        void start() throws TwitterException; //This method is responsible for starting the stream.
-
-}
